5c5dbca7829ca5167c3cc12dc4a3bde4df390a9e
Kevin Connors <connorsk@grinnell.edu>
2017-08-09 09:34:41 -0400

    mvp

--- fischer.js
+++ fischer.js
@@ -15,6 +15,24 @@
         assetsSuffix: '.png',
     }
 
+    var permArr = [],
+        usedChars = [];
+
+    function permute(input) {
+      var i, ch;
+      for (i = 0; i < input.length; i++) {
+        ch = input.splice(i, 1)[0];
+        usedChars.push(ch);
+        if (input.length == 0) {
+          permArr.push(usedChars.slice());
+        }
+        permute(input);
+        input.splice(i, 0, ch);
+        usedChars.pop();
+      }
+      return permArr;
+    };
+
     var drawTile = function(imgName, posX, posY) {
 
         var img = new Image();
@@ -24,34 +42,59 @@
         }
 
         var imgSrc = GLOBALS.assetsPrefix + imgName + GLOBALS.assetsSuffix;
-        console.log(imgSrc);
         img.src = imgSrc;
     }
 
-    var x = 0;
+    var canvasX = 0;
+    var canvasY = 0;
     var pieceOptions = _.clone(GLOBALS.backRowPieces);
 
-    // 1st Square: Any nonKing piece
-    var piece1 = _.sample(_.without(pieceOptions, 'k'));
-    drawTile('b' + piece1, x, 0);
-    drawTile('w' + piece1, x, GLOBALS.imageHeight * 7);
-    x += GLOBALS.imageWidth;
+    // Draws 8 Pawns on each side
+    _(8).times(function(i) {
+        canvasX = GLOBALS.imageWidth * i;
+        canvasY = GLOBALS.imageHeight;
+        drawTile('bp', canvasX, canvasY)
+        canvasY = GLOBALS.imageHeight * 6;
+        drawTile('wp', canvasX, canvasY)
+    });
 
-    // Draws 8 Black Pawns
-    for (var i = 0; i < 8; i++) {
-        var x = GLOBALS.imageWidth * i;
-        var y = GLOBALS.imageHeight;
-        drawTile('bp', x, y)
+    var allPossibilities = permute(GLOBALS.backRowPieces);
+
+    var chess960PossibilitiesWithReps = _.sortBy(_.filter(allPossibilities, function(s) {
+
+        var r1Index = _.indexOf(s, 'r');
+        var r2Index = _.lastIndexOf(s, 'r');
+
+        var kIndex = _.indexOf(s, 'k');
+
+        var b1Index = _.indexOf(s, 'b');
+        var b2Index = _.lastIndexOf(s, 'b');
+
+        // King between Rooks && Bishops on different colors
+        return (r1Index < r2Index && kIndex > r1Index && kIndex < r2Index) && (b1Index + b2Index) % 2 == 1;
+    }));
+
+    var chess960Possibilities = [];
+
+    for (var i = 0; i < 7680; i += 8) {
+        chess960Possibilities.push(chess960PossibilitiesWithReps[i]);
     }
-
-    // Draws 8 White Pawns
-    for (var i = 0; i < 8; i++) {
-        var x = GLOBALS.imageWidth * i;
-        var y = GLOBALS.imageHeight * 6;
-        drawTile('wp', x, y)
-    }
-
     
+    var numSelected = 203//_.random(0, chess960Possibilities.length);
+    var selectedPossibility = chess960Possibilities[numSelected];
+
+    console.log(numSelected)
+    document.getElementById("num-of-initial-state").innerHTML = numSelected;
+
+    console.log(selectedPossibility);
+
+    for (var i = 0; i < 8; i++) {
+        canvasX = GLOBALS.imageWidth * i;
+        canvasY = 0;
+        drawTile('b' + selectedPossibility[i], canvasX, canvasY);
+        canvasY = GLOBALS.imageHeight * 7;
+        drawTile('w' + selectedPossibility[i], canvasX, canvasY);
+    }
 
 })();
 
